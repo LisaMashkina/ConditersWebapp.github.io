9207d369bc616e4bdb2b71fc8c0e106fc0fd0d17
LisaMashkina <159134421+LisaMashkina@users.noreply.github.com>
2024-10-20 14:21:50 +0300

    Add files via upload

---
+++ MAINpdProject/script.js
@@ -0,0 +1,182 @@
+let tg = window.Telegram.WebApp;
+tg.expand();
+
+let popup = document.querySelector(".popup-js")
+
+function closePopup(){
+    popup.classList.remove('open-popup')
+}
+
+async function getResponce(){
+    let response = await fetch('https://jsonplaceholder.typicode.com/photos')
+    var content = await response.json()
+    // var content = result
+    function openPopup(){
+        key = content[this.id]
+        let popupJS = document.querySelector(".popup")
+        popup.classList.add('open-popup')
+        console.log(key)
+        
+        popupJS.innerHTML = `
+            <button class="close" type="submit" onclick="closePopup()">Назад</button>
+            <div class="popup-header">
+                <img src="${key.url}" alt="..." class="popup-img">
+                <div class="main-popap-inf">
+                <p>${key.title.slice(0, 20)}</p>
+                <a class="bt-to-cond" aria-current="page" href="personal-page.html" ">Кондитер</a>
+                </div>
+            </div>
+            <div class="popup-od-inf">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit
+            </div>
+            <div class="buy-container"><a class="popup-buy" href="#"><b>Заказать</b></a></div>
+        `
+    }
+
+    
+    console.log(content[0])
+    content = content.splice(0, 10)
+    let cardCondRight = document.querySelector(".right")
+    let cardCondLeft = document.querySelector(".left")
+    
+    let key; // условие ? выражение1 : выражение2
+    let a = 15;
+    for(key = 0; key < content.length; key += 1){
+        if (key%2 == 0){
+            cardCondRight.innerHTML += `
+            <div id="${key}">
+              <div class="card">
+                <img src="${content[key].url}" class="card-img-top" alt="mew">
+                <div class="card-body">
+                  <p class="card-text">${content[key].title.length < a? content[key].title.slice(0,a): content[key].title.slice(0,a) + '...' }</p>
+                </div>
+              </div>
+            </div>
+            `
+        }
+        
+        else { 
+            cardCondLeft.innerHTML += `
+            <div id="${key}">
+              <div class="card">
+                <img src="${content[key].url}" class="card-img-top" alt="mew">
+                <div class="card-body">
+                  <p class="card-text">${content[key].title.length < a? content[key].title.slice(0,a): content[key].title.slice(0,a) + '...' }</p>
+                </div>
+              </div>
+            </div>
+            `
+        }
+    }
+
+    for(key = 0; key < content.length; key += 1){
+        elem = document.getElementById(key)
+        elem.addEventListener('click', openPopup)
+    }
+
+}
+
+
+result = [
+  {
+    "albumId": 1,
+    "id": 1,
+    "title": "Месяц",
+    "url": "static/img/cake1.jpg",
+    "thumbnailUrl": "https://via.placeholder.com/150/92c952"
+  },
+  {
+    "albumId": 1,
+    "id": 2,
+    "title": "Шампанское",
+    "url": "static/img/cake3.jpg",
+    "thumbnailUrl": "https://via.placeholder.com/150/771796"
+  },
+  {
+    "albumId": 1,
+    "id": 3,
+    "title": "Самолетик",
+    "url": "static/img/cake2.jpg",
+    "thumbnailUrl": "https://via.placeholder.com/150/24f355"
+  },
+  {
+    "albumId": 1,
+    "id": 4,
+    "title": "Корзиночка",
+    "url": "static/img/cake4.jpg",
+    "thumbnailUrl": "https://via.placeholder.com/150/d32776"
+  },
+  {
+    "albumId": 1,
+    "id": 5,
+    "title": "natus nisi omnis corporis facere molestiae rerum in",
+    "url": "https://via.placeholder.com/600/f66b97",
+    "thumbnailUrl": "https://via.placeholder.com/150/f66b97"
+  },
+  {
+    "albumId": 1,
+    "id": 6,
+    "title": "accusamus ea aliquid et amet sequi nemo",
+    "url": "https://via.placeholder.com/600/56a8c2",
+    "thumbnailUrl": "https://via.placeholder.com/150/56a8c2"
+  },
+  {
+    "albumId": 1,
+    "id": 7,
+    "title": "officia delectus consequatur vero aut veniam explicabo molestias",
+    "url": "https://via.placeholder.com/600/b0f7cc",
+    "thumbnailUrl": "https://via.placeholder.com/150/b0f7cc"
+  },
+  {
+    "albumId": 1,
+    "id": 8,
+    "title": "aut porro officiis laborum odit ea laudantium corporis",
+    "url": "https://via.placeholder.com/600/54176f",
+    "thumbnailUrl": "https://via.placeholder.com/150/54176f"
+  },
+  {
+    "albumId": 1,
+    "id": 9,
+    "title": "qui eius qui autem sed",
+    "url": "https://via.placeholder.com/600/51aa97",
+    "thumbnailUrl": "https://via.placeholder.com/150/51aa97"
+  },
+  {
+    "albumId": 1,
+    "id": 10,
+    "title": "beatae et provident et ut vel",
+    "url": "https://via.placeholder.com/600/810b14",
+    "thumbnailUrl": "https://via.placeholder.com/150/810b14"
+  },
+  {
+    "albumId": 1,
+    "id": 11,
+    "title": "nihil at amet non hic quia qui",
+    "url": "https://via.placeholder.com/600/1ee8a4",
+    "thumbnailUrl": "https://via.placeholder.com/150/1ee8a4"
+  },
+  {
+    "albumId": 1,
+    "id": 12,
+    "title": "mollitia soluta ut rerum eos aliquam consequatur perspiciatis maiores",
+    "url": "https://via.placeholder.com/600/66b7d2",
+    "thumbnailUrl": "https://via.placeholder.com/150/66b7d2"
+  },
+  {
+    "albumId": 1,
+    "id": 13,
+    "title": "repudiandae iusto deleniti rerum",
+    "url": "https://via.placeholder.com/600/197d29",
+    "thumbnailUrl": "https://via.placeholder.com/150/197d29"
+  },
+  {
+    "albumId": 1,
+    "id": 14,
+    "title": "est necessitatibus architecto ut laborum",
+    "url": "https://via.placeholder.com/600/61a65",
+    "thumbnailUrl": "https://via.placeholder.com/150/61a65"
+  }
+]
+
+getResponce()
+
---
+++ MAINpdProject/selfPage.js
@@ -0,0 +1,145 @@
+let popup = document.querySelector(".popup-js")
+
+function closePopup(){
+    popup.classList.remove('open-popup')
+}
+
+async function selfpage(){
+    // let response = await fetch('https://osetiaalaniapum.ru/aa')
+    // var content = await response.json()
+    var content = res
+    function openPopup(){
+        key = content.cakes[this.id]
+        let popupJS = document.querySelector(".popup")
+        popup.classList.add('open-popup')
+   
+        popupJS.innerHTML = `
+            <button class="close" type="submit" onclick="closePopup()">Назад</button>
+            <div class="popup-header">
+                <img src="${key.photo}" alt="..." class="popup-img">
+                <div class="main-popap-inf">
+                <p>${key.name.slice(0, 20)}</p>
+                <a class="bt-to-cond" aria-current="page" href = "#" ">Кондитер</a>
+                </div>
+            </div>
+            <div class="popup-od-inf">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, itaque  iusto quisquam!
+            </div>
+        `
+    }
+    let selfBody = document.querySelector(".selfBody")
+    selfBody.innerHTML += `
+        
+        <div><button onclick="window.history.back()" class="back-butt">назад</button></div>
+        <div class="main-inf-block">
+            <div class="photo-profile">
+                <img src="${content.img}" alt="...">
+            </div>
+            <div class="inf-block">
+                <h2>${content.name}</h2>
+            </div>
+        </div>
+        <div class="description">
+        ${content.desc}
+        </div>
+        
+    `
+    let socialMedia = document.querySelector(".inf-block")
+    if (content.youtube){
+        socialMedia.innerHTML += `
+            <p>YouTube: ${content.youtube} </p>
+        `
+    }
+   
+    if (content.vk){
+        socialMedia.innerHTML += `
+            <p>VK: ${content.vk} </p>
+        `
+    }
+
+    if (content.inst){
+        socialMedia.innerHTML += `
+            <p>Опыт работы: ${content.inst} </p>
+        `
+    }
+    let cardCondRight = document.querySelector(".right")
+    let cardCondLeft = document.querySelector(".left")
+    let key; // условие ? выражение1 : выражение2 cont.cakes: [{}, {}]
+    let a = 15;
+    for(key = 0; key < content.cakes.length; key += 1){
+        if (key%2 == 0){
+            cardCondRight.innerHTML += `
+            <div id="${key}">
+              <div class="card">
+                <img src="${content.cakes[key].photo}" class="card-img-top" alt="mew">
+                <div class="card-body" style="ttext-align: center;">
+                  <p class="card-text">${content.cakes[key].name.length < a? content.cakes[key].name.slice(0,a): content.cakes[key].name.slice(0,a) + '...' }</p>
+                </div>
+              </div>
+            </div>
+            `
+        }
+        
+        else { 
+            cardCondLeft.innerHTML += `
+            <div id="${key}">
+              <div class="card">
+                <img src="${content.cakes[key].photo}" class="card-img-top" alt="mew">
+                <div class="card-body">
+                  <p class="card-text">${content.cakes[key].name.length < a? content.cakes[key].name.slice(0,a): content.cakes[key].name.slice(0,a) + '...' }</p>
+                </div>
+              </div>
+            </div>
+            `
+        }
+    }
+
+    for(key = 0; key < content.cakes.length; key += 1){
+        elem = document.getElementById(key)
+        elem.addEventListener('click', openPopup)
+    }
+}
+
+
+res = {
+    'img': 'static/img/cat.jpg',
+    'name': 'Анна Малюгина',
+    'inst': '2 года',
+    'desc': 'Кулинария - моё хобби и моя работа, люблю радовать своих заказчиков яркими дизайнами и вкусными тортами!',
+    'time': 'dd.mm.yyyy',
+    'youtube': 'url',
+    'vk': 'url',
+    'cakes': [
+        {
+            'name': 'Месяц',
+            'photo': 'static/img/cake1.jpg',
+            'desc': 'Декор: месяц на звёздном небе. Начинка: Шоколад-вишня 2000р/кг'
+        },
+        {
+            'name': 'Самолетик',
+            'photo': 'static/img/cake2.jpg',
+            'desc': 'Описание торт1'
+        },
+        {
+            'name': 'Название торта2',
+            'photo': 'https://via.placeholder.com/600/24f355',
+            'desc': 'Описание торт2'
+        },
+        {
+            'name': 'Название торта3',
+            'photo': 'https://via.placeholder.com/600/d32776',
+            'desc': 'Описание торт3'
+        },
+        {
+            'name': 'Название торта4',
+            'photo': 'https://via.placeholder.com/600/d32776',
+            'desc': 'Описание торт3'
+        },
+        {
+            'name': 'Название торта5',
+            'photo': 'https://via.placeholder.com/600/d32776',
+            'desc': 'Описание торт3'
+        },
+    ]
+}
+selfpage()
